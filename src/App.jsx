76694f8f2f3cfd3904ae7f7251bc8e80fef42731
Vickrant <vickrantselvendran@gmail.com>
2025-03-15 22:52:17 +0530

    fixed routing v10

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,20 @@ import Quiz from './Components/Quiz'
 import Results from './Components/Results'
 import Welcome from './Components/Welcome'
 import QuizCenter from './Components/QuizCenter'
-import { BrowserRouter as HashRouter, Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as HashRouter, Router, Routes, Route, BrowserRouter } from 'react-router-dom'
 
 export default function App() {
 
     return (
         <>
-            <HashRouter>
+            <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Welcome/>}></Route>
                     <Route path="/quizCenter" element={<QuizCenter/>}></Route>
                     <Route path="/quizCenter/quiz/:quizLink" element={<Quiz/>}></Route>
                     <Route path="/quizCenter/quiz/:quizLink/results" element={<Results/>}></Route>
                 </Routes>
-            </HashRouter>
+            </BrowserRouter>
         </>
     )
 
