9c32d44267508ee26764ca21293d65f1e34d0b43
Vickrant <vickrantselvendran@gmail.com>
2025-03-20 14:56:26 +0530

    Quiz 1 Completed - 1/4

--- src/utils/quizData.js
+++ src/utils/quizData.js
@@ -2,57 +2,440 @@ import quiz1Image1 from '../assets/road-narrows-ahead.svg'
 import quiz1Image2 from '../assets/slippery-road.svg'
 import quiz1Image3 from '../assets/road-closed.svg'
 import quiz1Image4 from '../assets/single-track-level-crossing.svg'
-import quiz1Image5 from '../assets/single-track-level-crossing.svg'
+import quiz1Image5 from '../assets/5.jpeg'
+import quiz1Image6 from '../assets/6.jpeg'
+import quiz1Image7 from '../assets/7.jpeg'
+import quiz1Image8 from '../assets/8.jpeg'
+import quiz1Image9 from '../assets/9.png'
+import quiz1Image10 from '../assets/10.png'
+import quiz1Image11 from '../assets/11.jpeg'
+import quiz1Image12 from '../assets/12.png'
+import quiz1Image13 from '../assets/13.png'
+import quiz1Image14 from '../assets/14.png'
+import quiz1Image15 from '../assets/15.png'
+import quiz1Image16 from '../assets/16.png'
+import quiz1Image17 from '../assets/17.png'
+import quiz1Image18 from '../assets/18.png'
+import quiz1Image19 from '../assets/19.png'
+import quiz1Image20 from '../assets/20.png'
+import quiz1Image21 from '../assets/21.png'
+import quiz1Image22 from '../assets/22.png'
+import quiz1Image23 from '../assets/23.png'
+import quiz1Image24 from '../assets/24.png'
+import quiz1Image25 from '../assets/25.png'
+import quiz1Image26 from '../assets/26.png'
+import quiz1Image27 from '../assets/27.png'
+import quiz1Image28 from '../assets/28.png'
+import quiz1Image29 from '../assets/29.png'
+import quiz1Image30 from '../assets/30.jpeg'
+import quiz1Image31 from '../assets/31.png'
+import quiz1Image32 from '../assets/32.jpeg'
+import quiz1Image33 from '../assets/33.png'
+import quiz1Image34 from '../assets/34.png'
+import quiz1Image35 from '../assets/35.jpeg'
+import quiz1Image38 from '../assets/38.png'
+import quiz1Image39 from '../assets/39.png'
+import quiz1Image40 from '../assets/40.png'
 
 export const quizData = [
     {
-        question: "This road sign depicts",
+        question: "This road sign depicts,",
         src: quiz1Image1,
         answers: [
-            {text: "Y junction ahead", correct: false},
-            {text: "Road narrows ahead", correct: true},
-            {text: "Dual carriageway ends ahead",  correct: false},
-            {text: "Narrow bridge ahead",  correct: false},
+            { text: "Dual carriage way starts ahead", correct: false },
+            { text: "Road narrows ahead", correct: true },
+            { text: "Dual carriage way ends ahead", correct: false },
+            { text: "Narrow Bridge ahead", correct: false },
         ]
     },
     {
-        question: "This road sign depicts",
+        question: "This road sign depicts,",
         src: quiz1Image2,
         answers: [
-            {text: "Slippery road ahead", correct: true},
-            {text: "Road with bends ahead", correct: false},
-            {text: "Dangerous junction ahead",  correct: false },
-            {text: "Road narrows ahead",  correct: false},
+            { text: "Slippery Road ahead", correct: true },
+            { text: "Road with bends ahead", correct: false },
+            { text: "Dangerous junction ahead", correct: false },
+            { text: "Road narrows ahead", correct: false },
         ]
     },
     {
-        question: "This road sign depicts",
+        question: "This road sign depicts,",
         src: quiz1Image3,
         answers: [
-            {text: "Compulsory roundabout", correct: false},
-            {text: "No Entry", correct: false},
-            {text: "Road closed for buses and lorries", correct: false},
-            {text: "All Vehicles Prohibited", correct: true},
+            { text: "Compulsory roundabout", correct: false },
+            { text: "No Entry", correct: false },
+            { text: "Road closed for buses and lorries", correct: false },
+            { text: "Road closed", correct: true },
         ]
     },
     {
-        question: "This road sign depicts",
+        question: "This road sign depicts,",
         src: quiz1Image4,
         answers: [
-            {text: "Road closed ahead", correct: false},
-            {text: "Railway crossing ahead", correct: false},
-            {text: "Single track level crossing", correct: true},
-            {text: "Unprotected railway crossing", correct: false},
+            { text: "Road closed ahead", correct: false },
+            { text: "Railway crossing ahead", correct: false },
+            { text: "Unprotected railway crossing", correct: true },
+            { text: "Unprotected railway crossing ahead", correct: false },
         ]
     },
     {
-        question: "The driver who enters the T junction depicted in the picture should",
+        question: "The driver who enters the T junction depicted in the picture should,",
         src: quiz1Image5,
         answers: [
-            {text: "Drive ahead if there are no vehicles on the main road", correct: false},
-            {text: "Stop immediately", correct: true},
-            {text: "Stop only if turning right", correct: false},
-            {text: "Stop only if turning left", correct: false},
+            { text: "Can drive ahead if there are no vehicles on the main road", correct: false },
+            { text: "It's mandatory to stop", correct: true },
+            { text: "Should stop only if turning right", correct: false },
+            { text: "Should not stop if turning left", correct: false },
         ]
-    }
-]
+    },
+    {
+        question: "This road marking depicts,",
+        src: quiz1Image6,
+        answers: [
+            { text: "Give way line", correct: false },
+            { text: "Stopping line at Road dividing", correct: false },
+            { text: "Stopping line at stop sign", correct: false },
+            { text: "Give way line at giveaway signs and at roundabouts", correct: true },
+        ]
+    },
+    {
+        question: "At the junction with traffic lights,",
+        src: quiz1Image7,
+        answers: [
+            { text: "If needed the Van can turn left", correct: false },
+            { text: "It's compulsory the van should turn right only", correct: true },
+            { text: "The motor car can turn right if needed", correct: false },
+            { text: "The motor car can turn left if needed", correct: false },
+        ]
+    },
+    {
+        question: "What is depicted by the road marking?",
+        src: quiz1Image8,
+        answers: [
+            { text: "Lane marking", correct: false },
+            { text: "Central Line marking", correct: false },
+            { text: "Longitudinal centre line shall be crossed only for turning right", correct: false },
+            { text: "Longitudinal double continuous centreline prohibiting crossing, turning right, and straddling", correct: true },
+        ]
+    },
+    {
+        question: "What is the command shown in the picture by the traffic police officer?",
+        src: quiz1Image9,
+        answers: [
+            { text: "Traffic approaching both from front and behind stop", correct: true },
+            { text: "Traffic from behind stop", correct: false },
+            { text: "Stop", correct: false },
+            { text: "Traffic approaching from front stop", correct: false },
+        ]
+    },
+    {
+        question: "What is the next colour that will light on in the signal light?",
+        src: quiz1Image10,
+        answers: [
+            { text: "Red and yellow", correct: false },
+            { text: "Red", correct: true },
+            { text: "Green and yellow", correct: false },
+            { text: "Green", correct: false },
+        ]
+    },
+    {
+        question: "If you are intending to turn left at the junction, what should be the relevant procedure at point A, B, and C,",
+        src: quiz1Image11,
+        answers: [
+            { text: "Decide, Signal, and act", correct: false },
+            { text: "Observe, decide, act", correct: true },
+            { text: "Signal, decide, act", correct: false },
+            { text: "All above are correct", correct: false },
+        ]
+    },
+    {
+        question: "At the junction depicted in the picture,",
+        src: quiz1Image12,
+        answers: [
+            { text: "It's compulsory that the car move forward", correct: true },
+            { text: "The trishaw can go forward", correct: false },
+            { text: "The trishaw can either turn right or move forward", correct: false },
+            { text: "The motor car can either turn right or move forward", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image13,
+        answers: [
+            { text: "Y junction ahead", correct: true },
+            { text: "Road narrows ahead", correct: false },
+            { text: "Side road from both ways intersects ahead", correct: false },
+            { text: "Dual carriage way starts ahead", correct: false },
+        ]
+    },
+    {
+        question: "What does this road sign indicate?",
+        src: quiz1Image14,
+        answers: [
+            { text: "Slippery road ahead", correct: false },
+            { text: "Dangerous ascent ahead", correct: false },
+            { text: "Slippery road for cars ahead", correct: false },
+            { text: "Dangerous descent ahead", correct: true },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image15,
+        answers: [
+            { text: "Pedestrian lane commences", correct: false },
+            { text: "Give way for pedestrians", correct: false },
+            { text: "Pedestrian crossing", correct: true },
+            { text: "Pedestrian crossing ahead", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image16,
+        answers: [
+            { text: "Road closed for all vehicles", correct: false },
+            { text: "No parking on odd days", correct: false },
+            { text: "No parking and standing", correct: false },
+            { text: "No parking", correct: true },
+        ]
+    },
+    {
+        question: "What does this road sign indicate?",
+        src: quiz1Image17,
+        answers: [
+            { text: "A prohibition symbol", correct: false },
+            { text: "Restriction symbol", correct: true },
+            { text: "Mandatory synmbol", correct: false },
+            { text: "Danger warning symbol", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image18,
+        answers: [
+            { text: "Double bend to left ahead", correct: false },
+            { text: "Turn right ahead", correct: false },
+            { text: "Right bend ahead", correct: true },
+            { text: "Hair pin bend to right ahead", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image19,
+        answers: [
+            { text: "Pedestrian crossing", correct: false },
+            { text: "Police station", correct: false },
+            { text: "Petrol filling station", correct: false },
+            { text: "Parking", correct: true },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image20,
+        answers: [
+            { text: "Children's crossing", correct: false },
+            { text: "Blind people crossing ahead", correct: false },
+            { text: "Children crossing ahead", correct: true },
+            { text: "School ahead", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image21,
+        answers: [
+            { text: " Four way junction ahead", correct: false },
+            { text: "Priority road", correct: true },
+            { text: " Priority road ahead", correct: false },
+            { text: "Junction box", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image22,
+        answers: [
+            { text: "Animal Sanctuary ahead", correct: false },
+            { text: "Cattle farm ahead", correct: false },
+            { text: "Cattle or other animals may cross the road ahead", correct: true },
+            { text: "Pedestrian crossing for cattle ahead", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image23,
+        answers: [
+            { text: "Give away", correct: true },
+            { text: "Compulsory triangle junction", correct: false },
+            { text: "Give away ahead", correct: false },
+            { text: "Main road ahead", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image24,
+        answers: [
+            { text: "Stop ahead", correct: false },
+            { text: "Cross roads ahead", correct: false },
+            { text: "Compulsory roundabout ahead", correct: false },
+            { text: "Roundabout ahead", correct: true },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image25,
+        answers: [
+            { text: "Turn ahead", correct: false },
+            { text: "Turn left", correct: false },
+            { text: "Pass this side", correct: true },
+            { text: "Turn left ahead", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image26,
+        answers: [
+            { text: "Y junction ahead", correct: false },
+            { text: "Main road ahead", correct: true },
+            { text: " Give priority for traffic from left", correct: false },
+            { text: "Give priority for traffic from", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image27,
+        answers: [
+            { text: "In to city limits", correct: false },
+            { text: "Speed limit", correct: true },
+            { text: "Minimum speed limit", correct: false },
+            { text: "End of speed limit", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image28,
+        answers: [
+            { text: "Main road ahead", correct: false },
+            { text: "Priority for traffic ahead", correct: false },
+            { text: "Priority over oncoming traffic", correct: true },
+            { text: "Watch out for the oncoming vehicles", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image29,
+        answers: [
+            { text: "Pedestrian crossing ahead", correct: true },
+            { text: "Crossing line ahead", correct: false },
+            { text: "Passenger crossing ahead", correct: false },
+            { text: "Children crossing ahead", correct: false },
+        ]
+    },
+    {
+        question: "What is depicted by the horizontal line in the road marking?",
+        src: quiz1Image30,
+        answers: [
+            { text: "Give way sign", correct: false },
+            { text: "Stop line at stop signs or at traffic signals", correct: true },
+            { text: "Stop line at stop signal", correct: false },
+            { text: "Stop signal at roundabout", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image31,
+        answers: [
+            { text: "Main Road ahead", correct: false },
+            { text: "Give away ahead", correct: true },
+            { text: "Triangular junction ahead", correct: false },
+            { text: "Give away", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image32,
+        answers: [
+            { text: "Speed limit", correct: true },
+            { text: "Average speed", correct: false },
+            { text: "Minimum speed", correct: false },
+            { text: "Speed limit in city limits", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image33,
+        answers: [
+            { text: "Double bend to left head", correct: false },
+            { text: "Hair pin bend to left head", correct: true },
+            { text: "Bend to left ahead", correct: false },
+            { text: "U turning head", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image34,
+        answers: [
+            { text: "Dual carriageway starts ahead", correct: false },
+            { text: "Road narrows ahead", correct: false },
+            { text: "Dual carriageway ends ahead", correct: true },
+            { text: "Narrow Bridge ahead", correct: false },
+        ]
+    },
+    {
+        question: "The horizontal line marked in the picture depicts,",
+        src: quiz1Image35,
+        answers: [
+            { text: "Give away line", correct: false },
+            { text: "Line to stop when operating traffic lights off by traffic police", correct: false },
+            { text: "Stop line at stop signs", correct: true },
+            { text: "Give away sign at roundabouts", correct: false },
+        ]
+    },
+    {
+        question: "What is depicted by the road marking?",
+        src: quiz1Image8,
+        answers: [
+            { text: "Lane marking", correct: false },
+            { text: "Central Line marking", correct: false },
+            { text: "Longitudinal centre line shall be crossed only for turning right", correct: false },
+            { text: "Longitudinal double continuous centreline prohibiting crossing turning right and straddling", correct: true },
+        ]
+    },
+    {
+        question: "At the junction depicted in the pictures,",
+        src: quiz1Image12,
+        answers: [
+            { text: "It's compulsory that the car move forward", correct: true },
+            { text: "The trishaw can go forward", correct: false },
+            { text: "The trishaw car can either turn right over move forward", correct: false },
+            { text: "The motor car can either turn right over move forward", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image38,
+        answers: [
+            { text: "Weight limit on one axle", correct: false },
+            { text: "End of speed limit", correct: true },
+            { text: "Beginning of speed limit", correct: false },
+            { text: "Speed limit", correct: false },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image39,
+        answers: [
+            { text: "Double bend to right ahead", correct: false },
+            { text: "U turn ahead", correct: false },
+            { text: "Right bend ahead", correct: false },
+            { text: "Hair pin bend to right ahead", correct: true },
+        ]
+    },
+    {
+        question: "This road sign depicts,",
+        src: quiz1Image40,
+        answers: [
+            { text: "Entry prohibited on even days", correct: false },
+            { text: "No parking on Odd days", correct: false },
+            { text: "No parking on even days", correct: true },
+            { text: "Entry prohibited on odd days", correct: false },
+        ]
+    },
+];
